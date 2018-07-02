5b88087be91c57c0126544bdb68a2562db7ca48d
Rita Sullivan <ritarojas@mac.com>
2018-07-02 16:41:13 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,13 +1,11 @@
 # birthday_kids = {
-#   "Timmy" => 9,
-#   "Sarah" => 6,
+#   "Timmy" => 9, 
+#   "Sarah" => 6, 
 #   "Amanda" => 27
 # }
 
 def happy_birthday(birthday_kids)
-  birthday_kids = {
-    "Timmy" => 9,
-    "Sarah" => 6,
-    "Amanda" => 27
-}
+  # add your code snippet here!
 end
+
+
